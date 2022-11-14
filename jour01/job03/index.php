c0d5984149cb41f33d9df33621cd52640660af6e
maxime-billon-grand <maxime.billon@laplateforme.io>
2022-11-14 14:11:50 +0100

    Mise en forme du tableau

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -5,6 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table, th, td{
+            border: 2px solid black;
+            text-align:center;
+        }
+
+        td, th{
+            padding-left:1em;
+            padding-right:1em;
+        }
+
+    </style>
 </head>
 <body>
     <?php 
@@ -15,35 +27,31 @@
     ?> 
 
     <table>
-        <thead>
-            <tr>
-                <th>Type</th>
-                <th>Nom</th>
-                <th>Valeur</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>String</td>
-                <td>$str</td>
-                <td><?php echo $str;?></td>
-            </tr>
-            <tr>
-                <td>Integer</td>
-                <td>$int</td>
-                <td><?php echo $int;?></td>
-            </tr>
-            <tr>
-                <td>Float</td>
-                <td>$float</td>
-                <td><?php echo $float;?></td>
-            </tr>
-            <tr>
-                <td>Boolean</td>
-                <td>$bool</td>
-                <td><?php echo $bool;?></td>
-            </tr>
-        </tbody>
+        <tr>
+            <th>Type</th>
+            <th>Nom</th>
+            <th>Valeur</th>
+        </tr>
+        <tr>
+            <td>String</td>
+            <td>$str</td>
+            <td><?php echo $str;?></td>
+        </tr>
+        <tr>
+            <td>Integer</td>
+            <td>$int</td>
+            <td><?php echo $int;?></td>
+        </tr>
+        <tr>
+            <td>Float</td>
+            <td>$float</td>
+            <td><?php echo $float;?></td>
+        </tr>
+        <tr>
+            <td>Boolean</td>
+            <td>$bool</td>
+            <td><?php echo $bool;?></td>
+        </tr>
     </table>
 </body>
 </html>
