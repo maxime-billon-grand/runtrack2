b44e91c26dd34a1bcf9d463c88c56da261bb78cc
maxime-billon-grand <maxime.billon@laplateforme.io>
2022-11-15 18:16:28 +0100

    Correction Job 05

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -7,16 +7,30 @@
     <title>Jour 02 - Job 05</title>
 </head>
 <body>
-    <?php 
+    <?php
+
+    function testPremier($a, $nbr){
+        $prime = True;
+        foreach($a as $nbrTest){
+            if ($nbr % $nbrTest == 0){
+                $prime = False;
+            }
+        }
+        
+        return $prime;
+    }
+
+    $prime_array = array(2, 3, 5, 7);
+
     for ($i = 2; $i <= 1000; $i++) {
-        if ($i === 2 || $i === 3 || $i === 5 || $i === 7){
+        if ($i === 2 || $i === 3|| $i === 5|| $i === 7){
             echo "<p>$i</p>";
         }
-        elseif ($i % 2 !== 0 && $i % 3 !== 0 && $i % 5 !== 0 && $i % 7 !== 0){
+        elseif (testPremier($prime_array, $i)){
             echo "<p>$i</p>";
+            $prime_array[] = $i;
         }
-        else{
-        }
+
 
     }   
     
