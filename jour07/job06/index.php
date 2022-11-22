0eef33bdb10b7ac83582bef6cd5950c100a0a79f
maxime-billon-grand <maxime.billon@laplateforme.io>
2022-11-22 12:49:18 +0100

    Fin Jour 07 Job 06

--- jour07/job06/index.php
+++ jour07/job06/index.php
@@ -7,6 +7,47 @@
     <title>Jour 07 - Job 06</title>
 </head>
 <body>
-    
+    <?php
+    function leetSpeak($str){
+        for($i=0; isset($str[$i]); $i++){
+            switch ($str[$i]){
+                case "a":
+                case "A":
+                    $str[$i] = "4";
+                    break;
+                case "b":
+                case "B":
+                    $str[$i] = "8";
+                    break;
+                case "e":
+                case "E":
+                    $str[$i] = "3";
+                    break;
+                case "g":
+                case "G":
+                    $str[$i] = "6";
+                    break;
+                case "l":
+                case "L":
+                    $str[$i] = "1";
+                    break;
+                case "s":
+                case "S":
+                    $str[$i] = "5";
+                    break;
+                case "t":
+                case "T":
+                    $str[$i] = "7";
+                    break;
+            }
+        }
+
+        return $str;
+    }
+
+    echo leetSpeak("ABCDEF");
+    ?>
+
+
 </body>
 </html>
